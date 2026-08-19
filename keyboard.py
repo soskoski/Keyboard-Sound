8ccc564eacf44ad7d4926c05421ecddee403ab40
David Shoshkoski <soskoski@gmail.com>
2026-08-19 19:08:51 +0200

    added mute, working on unmute

--- keyboard.py
+++ keyboard.py
@@ -8,20 +8,50 @@ SOUND_SHOT =  os.path.join(BASE_DIR, "audio/gunshot.mp3")
 SOUND_RELOAD = os.path.join(BASE_DIR, "audio/reload.mp3")
 
 pygame.mixer.init()
-shot = pygame.mixer.Sound(SOUND_SHOT)
-reload = pygame.mixer.Sound(SOUND_RELOAD)
+shot_sound = pygame.mixer.Sound(SOUND_SHOT)
+reload_sound = pygame.mixer.Sound(SOUND_RELOAD)
 
 ignored_keys = [keyboard.Key.space, keyboard.Key.backspace]
 
-def on_press(key):
+def mute():
+    pygame.mixer.pause()
+    print("Muted!\n")
+
+def unmute():
+    pygame.mixer.unpause()
+    print("Unmute!\n")
+
+pause = keyboard.HotKey(keyboard.HotKey.parse('<ctrl>+1'), mute)
+unpause = keyboard.HotKey(keyboard.HotKey.parse('<ctrl>+1'), unmute)
+is_ctrl_pressed = False
+
+def handle_press(key):
+
+    global is_ctrl_pressed
+
+    if key in (keyboard.Key.ctrl, keyboard.Key.ctrl_l, keyboard.Key.ctrl_r):
+        is_ctrl_pressed = True
+
+    canonical_key = listener.canonical(key)
+    pause.press(canonical_key)
+
+    if is_ctrl_pressed:
+        return
+
     if key in ignored_keys:
         return
 
     if key == keyboard.Key.enter:
-        pygame.mixer.Sound.play(reload)
+        pygame.mixer.Sound.play(reload_sound)
         return
     
-    pygame.mixer.Sound.play(shot)
+    pygame.mixer.Sound.play(shot_sound)
 
-with keyboard.Listener(on_press=on_press) as listener:
+def handle_release(key):
+    canonical_key = listener.canonical(key)
+    pause.release(canonical_key)
+
+listener = keyboard.Listener(on_press=handle_press, on_release=handle_release)
+
+with listener:
     listener.join()
